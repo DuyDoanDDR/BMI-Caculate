662e568a4ebd6fa9b392ea0f8b1566746ce8b2cd
Doan Minh Duy <duydoan812@gmail.com>
2025-03-15 12:05:14 +0700

    Update_01 Commit

--- Program.cs
+++ Program.cs
@@ -4,38 +4,30 @@ double weight;
 //Nhap va kiem tra gia tri user nhap vao da hop ly chua
 Console.WriteLine("Enter Height as meters :");
 while (true)
-    if (double.TryParse(Console.ReadLine(), out height))
+    if (double.TryParse(Console.ReadLine(), out height) && height > 0)
     {
-        if (height > 0)
-        {
-            break;
-        }
-        else Console.WriteLine("Enter Height as a positive number which greater than 0");
+        break;
     }
     else Console.WriteLine("Enter Height as a positive number which greater than 0");
 Console.WriteLine("Enter Weight as kilograms :");
 while (true)
-    if (double.TryParse(Console.ReadLine(), out weight))
+    if (double.TryParse(Console.ReadLine(), out weight) && weight >0)
     {
-        if (weight > 0)
-        {
-            break ;
-        }
-        else Console.WriteLine("Enter Weight as a positive number which greater than 0");
+        break;
     }
     else Console.WriteLine("Enter Weight as a positive number which greater than 0");
 double BMI = weight / Math.Pow(height, 2);
-BMI = Math.Round(BMI,1);
+BMI = Math.Round(BMI, 1);
 Console.WriteLine("BMI :" + BMI);
 
 //Kiem tra, danh gia va xuat ket qua dua tren BMI da tinh
-if (BMI >= 30 )
+if (BMI >= 30)
 {
     Console.WriteLine("Obese");
 }
 else
 {
-    if (BMI >= 25 )
+    if (BMI >= 25)
     {
         Console.WriteLine("Overweight");
     }
